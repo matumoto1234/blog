9e65f2ddfe4cbe96b29efa1dd5f178c025719878
matumoto1234 <matumotosizuoka@gmail.com>
2024-01-10 22:03:21 +0900

    feat: 文章の行間を空けて見やすくした #35

--- src/utils/components/MarkdownViewer/components/Paragraph/index.css.ts
+++ src/utils/components/MarkdownViewer/components/Paragraph/index.css.ts
@@ -4,7 +4,7 @@ import { fontSize, fontWeight, lineHeight } from '@/utils/styles/text.css'
 export const paragraph = style([
   fontSize.px16,
   fontWeight.normal,
-  lineHeight.per200,
+  lineHeight.per300,
   {
     marginBottom: '16px',
     textAlign: 'justify',
